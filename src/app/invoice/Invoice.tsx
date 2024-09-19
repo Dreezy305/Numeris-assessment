1e39a99cb614cc8b8f66af21fed3ff417d6e16b9
dreezy305 <bankoleidris@gmail.com>
2024-09-19 10:41:46 +0100

    route fixes

--- src/app/invoice/Invoice.tsx
+++ src/app/invoice/Invoice.tsx
@@ -1,25 +1,41 @@
-import { Button, Flex, Space, Typography } from "antd";
+import { Button, Card, Flex, Space, Typography } from "antd";
 
 const Invoice = (): JSX.Element => {
   return (
-    <Flex
-      gap={6}
-      style={{ width: "100%" }}
-      className="mt-5"
-      justify="space-between"
-      align="center"
-    >
-      <Typography.Text className="font-NeueHaasDisplayMedium font-medium text-NumerisDark text-3xl">
-        Invoice
-      </Typography.Text>
-      <Space direction="horizontal" size={"small"}>
-        <Button type="default" shape="round" size={"large"} className="uppercase h-16 w-52 cursor-pointer">
-          see what’s new
-        </Button>
-        <Button type="primary" shape="round" size={"large"} className="uppercase h-16 w-52 cursor-pointer">
-          CREATE
-        </Button>
-      </Space>
+    <Flex vertical gap={6} style={{ width: "100%" }}>
+      <Flex
+        gap={6}
+        style={{ width: "100%" }}
+        className="mt-5"
+        justify="space-between"
+        align="center"
+      >
+        <Typography.Text className="font-NeueHaasDisplayMedium font-medium text-NumerisDark text-3xl">
+          Invoice
+        </Typography.Text>
+        <Space direction="horizontal" size={"middle"}>
+          <Button
+            type="default"
+            shape="round"
+            size={"large"}
+            className="uppercase h-16 w-52 cursor-pointer bg-white border-none text-NumerisGrey font-NeueHaasDisplayMedium text-sm"
+          >
+            see what’s new
+          </Button>
+          <Button
+            type="primary"
+            shape="round"
+            size={"large"}
+            className="uppercase h-16 w-52 cursor-pointer bg-NumerisBlue font-NeueHaasDisplayMedium text-sm text-white"
+          >
+            CREATE
+          </Button>
+        </Space>
+      </Flex>
+
+      <div className="grid grid-cols-4 gap-x-4">
+        <Card />
+      </div>
     </Flex>
   );
 };
