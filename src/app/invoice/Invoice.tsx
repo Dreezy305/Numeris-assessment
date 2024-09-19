d664ed97f15f2150431850a7b2378334ed83d69a
dreezy305 <bankoleidris@gmail.com>
2024-09-19 01:47:31 +0100

    app layout and invoice

--- src/app/invoice/Invoice.tsx
+++ src/app/invoice/Invoice.tsx
@@ -1,11 +1,24 @@
-import { Flex, Space, Typography } from "antd";
+import { Button, Flex, Space, Typography } from "antd";
 
 const Invoice = (): JSX.Element => {
   return (
-    <Flex gap={6} style={{ width: "100%" }} className="mt-5">
-      <Typography.Text className="">Invoice</Typography.Text>
+    <Flex
+      gap={6}
+      style={{ width: "100%" }}
+      className="mt-5"
+      justify="space-between"
+      align="center"
+    >
+      <Typography.Text className="font-NeueHaasDisplayMedium font-medium text-NumerisDark text-3xl">
+        Invoice
+      </Typography.Text>
       <Space direction="horizontal" size={"small"}>
-
+        <Button type="default" shape="round" size={"large"} className="uppercase h-16 w-52 cursor-pointer">
+          see what’s new
+        </Button>
+        <Button type="primary" shape="round" size={"large"} className="uppercase h-16 w-52 cursor-pointer">
+          CREATE
+        </Button>
       </Space>
     </Flex>
   );
